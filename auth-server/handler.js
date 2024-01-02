0406d93ad47aa44467ede835e7eafeddef01c375
Uaaron Sihan <uaaronsihan@gmail.com>
2024-01-02 00:43:06 -0800

    added get-events functionality

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -73,3 +73,39 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+module.exports.getCalendarEvents = async (event) => {
+
+  const access_token = decodeURIComponent(`${event.pathParmeters.code}`)
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return JSON.stringify({ events: results.data.items })
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+
+};
