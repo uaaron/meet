60501c2da2de2560a507b6241258210079998d27
Uaaron Sihan <uaaronsihan@gmail.com>
2024-01-17 13:48:06 -0800

    created tests for EventList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
