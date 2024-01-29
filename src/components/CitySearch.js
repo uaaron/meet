bf0965b72635bd14be1190ef88be1aefc9e44f20
Uaaron Sihan <uaaronsihan@gmail.com>
2024-01-28 22:18:16 -0800

    completed feature 1 unit testing

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -14,6 +14,13 @@ const CitySearch = ({ allLocations }) => {
     setQuery(value);
     setSuggestions(filteredLocations);
   }
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // hides list
+  }
+
   return (
     <div id="city-search">
       <input
@@ -27,9 +34,9 @@ const CitySearch = ({ allLocations }) => {
       {showSuggestions ?
         <ul className="suggestions">
           {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
+            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
           })}
-          <li key='See all cities'>
+          <li onClick={handleItemClicked} key='See all cities'>
             <b>See all cities</b>
           </li>
         </ul>
