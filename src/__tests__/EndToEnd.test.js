f26e4c54fdd70efda5f6f981cb6f064005dac839
Uaaron Sihan <uaaronsihan@gmail.com>
2024-02-23 12:18:18 -0800

    added skeleton for filterEvents endtoend test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -12,22 +12,53 @@ describe('show/hide an event details', () => {
 
   afterAll(() => {
     browser.close();
-  })
+  });
 
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
-  })
+  });
 
   test('User can expand an event to see its details', async () => {
     await page.click('.event #show-details');
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeDefined();
-  })
+  });
 
   test('User can collapse an event to hide details', async () => {
     await page.click('.event #show-details');
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
   })
-}); 
+});
+
+// bonus task
+/*
+describe('filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('user sees events from all cities', () => {
+
+  });
+
+  test('User sees a list of suggestions when typing a specific city name', () => {
+
+  })
+
+  test('User can select a city from a city from the suggested list'() => {
+
+  })
+})
+
+*/
